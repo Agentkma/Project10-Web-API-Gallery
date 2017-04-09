796765411bfbcce3f818953e9906074199a2c346
KevinMAnderson <kma2k4@hotmail.com>
2017-04-09 15:11:15 -0400

    project 1st commit
    1st commit to git hub…..ajax request not working…asking for help via
    slack

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// $(‘button’).click(function () {  // adds event handler/ click to any album/pic on html
+//
+// $(“button”).removeClass(“selected”);  //  removethe class “selected” of any button
+//
+// $(this).addClass(“selected); // add “selected” class to button actually clicked
+//
+
+
+//
+// }); //end click album pic/cover event function
+// var album_art=[];
+
+var spotifyAPI = "https://api.spotify.com/v1/albums?jsoncallback=?";
+
+var spotifyOptions = {
+images: [],
+genres: "Post-Grunge"  //// ids
+};
+
+
+
+
+
+var displayAlbums =function (data) {
+
+	var albumHMTL = '<ul>';  // start building the ul list for the album_art images
+
+	$.each(data.albums, function ( i, album) {
+	//this $.each accesses the spotify object /array and loops through each one…
+/// $.each (object/array, callBackFunction(); ///
+
+///the callBackFunction takes 2 arguments function ( i , value)  where i is the index value of the current array item, and value is the value of the current array item
+    	 albumHTML += '<li class = “grid-25 tablet-grid-50”>';  //add CSS class
+    	 albumHTML += ' <a href= " ' + album.href + ' "  class = "image" >' ; // .href is the prop needed to access the link
+    	 albumHTML += '<img src= "'+ album.images[1].url + ' " >  </a> </li>'; //.images.url is the prop in the spotify json object with the pic/jpg
+    });//END anonymous funcition
+
+      albumHTML += ' </ul>';
+
+      $("#album-gallery").html(albumHTML);
+
+  }; /// END displayAlbums functions
+
+
+$.getJSON(spotifyAPI, spotifyOptions, displayAlbums); // AJAX request with JQUERY
