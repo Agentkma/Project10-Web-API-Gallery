a7cd01a606854c8ade72cf2c0ac7a798f0d3e5d3
KevinMAnderson <kma2k4@hotmail.com>
2017-04-26 09:23:00 -0400

    7.0 api troubleshooting
    tried to implement search Spotify function…retirieving sear area
    values…split them into individual strings, remove white spaces, add +
    between terms…

--- js/script.js
+++ js/script.js
@@ -1,48 +1,74 @@
 
 $(document).ready(function() {
-// $(‘button’).click(function () {  // adds event handler/ click to any album/pic on html
-//
-// $(“button”).removeClass(“selected”);  //  removethe class “selected” of any button
-//
-// $(this).addClass(“selected); // add “selected” class to button actually clicked
-//
 
-//
-// }); //end click album pic/cover event function
-// var album_art=[];
+	var spotifyAPI = "https://api.spotify.com/v1/search"; //API
+	var spotifyOptions = {
+	q : eachSearchValue,
+	type :  "artists"
+	};
+	var displayAlbums =function (data) {
 
-var spotifyAPI = "https://api.spotify.com/v1/albums/?ids=41MnTivkwTO3UUJ8DrqEJJ,2rDBeHRM12UkqAsPdKSsKN,6w0aKN5a2bNvWGnlCiF29M,3HCMJ6JooRz54AOchWgmia,5ikzwtrIZlC4mfMXXxiDLh"; //API
+		var albumHTML = "<ul>";  // start building the ul list for the album_art images
 
-var spotifyOptions = {
-// ids: ["41MnTivkwTO3UUJ8DrqEJJ","2rDBeHRM12UkqAsPdKSsKN", "6w0aKN5a2bNvWGnlCiF29M","3HCMJ6JooRz54AOchWgmia", "5ikzwtrIZlC4mfMXXxiDLh"]
-// genres: "41MnTivkwTO3UUJ8DrqEJJ" //// ids
-};
+		$.each(data.items, function ( i, eachItem) {
+		//this $.each accesses the spotify object /array and loops through each one…
+		/// $.each (object/array, callBackFunction(); ///
+		///the callBackFunction takes 2 arguments function ( i , value)  where i is the index value of the current array item, and value is the value of the current array item
 
-var displayAlbums =function (data) {
+				 albumHTML += '<li class = “grid-25 tablet-grid-50”>';  //add CSS class
+				 albumHTML += ' <a href= " ' + eachItem.artists.href + ' "  class = "image" >' ; // .href is the prop needed to access the link
+				 albumHTML += '<img src= "' + eachItem.artists.images[1].url + ' " >  </a> </li>'; //.images.url is the prop in the spotify json object with the pic/jpg
 
-	var albumHTML = "<ul>";  // start building the ul list for the album_art images
+				 albumHTML += "</ul>";
+			});//END anonymous function
 
-	$.each(data.items, function ( i, eachItem) {
-	//this $.each accesses the spotify object /array and loops through each one…
-	/// $.each (object/array, callBackFunction(); ///
-	///the callBackFunction takes 2 arguments function ( i , value)  where i is the index value of the current array item, and value is the value of the current array item
-
-			 albumHTML += '<li class = “grid-25 tablet-grid-50”>';  //add CSS class
-    	 albumHTML += ' <a href= " ' + eachItem.albums.href + ' "  class = "image" >' ; // .href is the prop needed to access the link
-    	 albumHTML += '<img src= "' + eachItem.albums.images[1].url + ' " >  </a> </li>'; //.images.url is the prop in the spotify json object with the pic/jpg
-
-			 albumHTML += "</ul>";
-		});//END anonymous function
-
-	  $("#album-gallery").html(albumHTML);
-  }; /// END displayAlbums functions
+			$("#album-gallery").html(albumHTML);
+		}; /// END displayAlbums functions
 
 
 
-$.getJSON(spotifyAPI, spotifyOptions, displayAlbums).fail(function (jqXHR) {
-		console.log(jqXHR.status);
-		console.log(jqXHR.statusText);
 
-}); // AJAX request with JQUERY
+		var searchValues;
+		var eachSearchValue="q=";
+
+		$('textarea').focus( function () {
+        ///below not working in safari
+        $(this).css("outline-color", "#FF0000");
+    });//end focus function
+
+
+		$('button').click(function () {  // adds event handler/ click to submit button
+			$(this).submit( function (evt) {
+       // when form button submitted this code will run
+       //Stop the form from submitting //prevent default browser action
+			 evt.preventDefault();
+		 	});
+
+			///Remove / add classes not working
+			$(this).removeClass('selected');  //  removethe class “selected” of any button
+			$(this).addClass('selected'); // add “selected” class to button actually clicked
+			//jQuery
+			searchValues = $("#search-area").val();
+			//javascript SPLIT s
+			searchValues = searchValues.split("");
+			//Jquery remove white spaces at begnning & end of strings
+			searchValues = $(this).trim();
+			console.log(searchValues);
+			//join ("+")TO PUT THE STRINGS BACK TOGETHER WITH A + BETWEEN WORDS
+			eachSearchValue = searchValues.join("+");
+
+			$.getJSON(spotifyAPI,spotifyOptions,displayAlbums).fail(function (jqXHR) {
+					console.log(jqXHR.status);
+					console.log(jqXHR.statusText);
+			}); // AJAX request with JQUERY
+
+
+		}); //end click  event function
+
+
+
+
+
+
 
 }); ///END DOCUMENT READY FUNCTION
