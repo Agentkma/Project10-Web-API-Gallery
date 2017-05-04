584e715d151aa9d1bb9a1d933d92c561b01cd255
KevinMAnderson <kma2k4@hotmail.com>
2017-05-04 15:23:39 -0400

    lightbox plugin installed but bug in display

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 
 $(document).ready(function() {
 
+/*********************VARIABLES***************************************/
+
+// var $overlay =$('<div id="overlay"></div');
+// var $image=$("<img>");
+// var $caption=$("<div><p></p></div>");
 
 
-/**********************************************/
+
+
+
+/*********************MAIN SEARCH BOX FUNCTION*************************/
 
 
 
@@ -14,50 +22,60 @@ $(document).ready(function() {
     });//end focus function
 
 
-		$('button').click(function (evt) {  // adds event handler/ click to submit button
-       // when form button submitted this code will run
+		$('form').submit(function (evt) {  // adds event handler/ click to submit button
 
+			 //Stop the form from submitting //prevent default browser action...leaving page
+			 	 evt.preventDefault();
 
-			 		var searchValues;
+			 		var $submitButton = $("submit-button");
+			 		var $searchValues = $("#search-area");
 			 		var eachSearchValue;
-       //Stop the form from submitting //prevent default browser action
-			 		evt.preventDefault();
+					// when form button submitted this code will run
 
+					// disable the search-area while ajax getting info
+					$searchValues.prop("disabled",true);
+					//give the submit button a "searching label"
+					$submitButton.attr("disabled", true).val("searching...");
 					///Remove / add classes not working
 					$('button').removeClass('selected');  //  removethe class “selected” of any button
 					$(this).addClass('selected'); // add “selected” class to button actually clicked
 					//jQuery to get search area text and then remove white spaces at begin/end of string
-					searchValues = $("#search-area").val().trim();
 
-					eachSearchValue=searchValues;
+					eachSearchValue = $searchValues.val().trim();
 					//
 					// console.log(eachSearchValue);
 
 					var spotifyAPI 			= "https://api.spotify.com/v1/search"; //API
 					var spotifyOptions 	= {
 							q : eachSearchValue,
-							type :  "artist"
+							type :  "album"
 							};
 					function displayAlbums (data) {
 						console.log(data);
 						// console.log(data.items);
-							var albumHTML = '<ul>';  // start build ul list for the album_art images
+							var albumHTML = '<ul id="search-list">';  // start build ul list for the album_art images
 
-							$.each(data, function ( i, eachArtist) {
-								console.log(eachArtist);
-								console.log(eachArtist.items);
+							$.each(data.albums.items, function ( i, eachAlbum) {
+								// console.log(data.albums.items);
+
 							// $.each accesses the spotify object /array and loops through each one…
-							/// $.each (object/array, callBackFunction(); ///
-							///the callBackFunction takes 2 arguments function ( i , value)  where i is the index value of the current array item, and eachArtis is the value of the current array item
-									 albumHTML += '<li class = “grid-25 tablet-grid-50”>';  //add CSS class
-									 albumHTML += '<a href= " ' + eachArtist.items.href + ' " class = "image" >'; // .href is the prop needed to access the link
-									 console.log(albumHTML);
-									 albumHTML += '<img src= "' + eachArtist.items.images[1].url + ' " >  </a> </li>'; //.images.url is the prop in the spotify json object with the pic/jpg
-							});//END $.each
+							// /// $.each (object/array, callBackFunction(); ///
+							// ///the callBackFunction takes 2 arguments function ( i , value)  where i is the index value of the current array item, and eachArtis is the value of the current array item
+									 albumHTML += '<li class = “grid-1 grid-3”>';  //add CSS class
+									 albumHTML += '<a href= " ' + eachAlbum.href + ' " class = "image" data-lightbox="image" data-title = "' +  eachAlbum.name + ' "> '; // .href is the prop needed to access the link
 
+									 albumHTML += '<img src= "' + eachAlbum.images[0].url + ' " >  </a> </li>'; //.images.url is the prop in the spotify json object with the pic/jpg
+							});//END $.each
+							//
 								 albumHTML += '</ul>';
-										// 	console.log(albumHTML);
+											console.log(albumHTML);
 								$("#album-gallery").html(albumHTML);
+
+								// re-enable the search-area while ajax getting info
+								$searchValues.prop("disabled",false);
+								//give the submit button a "search"
+								$submitButton.attr("disabled", false).val("Search");
+
 						} /// END displayAlbums functions
 
 
@@ -65,7 +83,10 @@ $(document).ready(function() {
 
 					 $('#search-area').val("");
 
-		}); //end click  event function
+		}); //end  form buttonclick  event function
+
+
+/*************************CLICK ON ALBUM ART TO SEE LIGHTBOX OF INFO********************************/
 
 
 }); ///END DOCUMENT READY FUNCTION
